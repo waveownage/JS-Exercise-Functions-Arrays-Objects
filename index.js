3682c23830b2a1636c8a9b717419db580471f9cc
Andre Freeman <asf@mail.usf.edu>
2020-01-14 23:06:43 -0500

    makeSmartPerson done

--- index.js
+++ index.js
@@ -77,10 +77,22 @@ function getName(x) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(x) {
+  const object = {
+    name : x,
+    function sum(a, b) {
+      return a + b;
+    },
+    function speak() {
+      return `Hello, my name is ${x}`
+    }, 
+  }
+  return object;
 }
 
+
+
+ 
 
 
 
